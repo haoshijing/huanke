2bee3bac2c40e7a2e3eb986871a55f5fc74cdabb
郝仕兢 <haoshijing@corp.netease.com>
2018-03-26 16:13:59 +0800

    导入项目

---
+++ gateway/src/main/java/com/huanke/iot/gateway/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.huanke.iot.gateway;
+
+/**
+ * @author haoshijing
+ * @version 2018年03月26日 16:01
+ **/
+public class ApplicationStarter {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+###
+项目结构
+ * base ---公共模块
+ * gateway -- 和mqttserver接收和发送模块
+ * manage -- 管理后台
+ * job ---定时任务
