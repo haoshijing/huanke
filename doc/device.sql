e7ca6982771fad1d4c21b33986fc918a7b837f04
郝仕兢 <haoshijing690@163.com>
2018-03-27 07:29:40 +0800

    数据库修改

--- doc/device.sql
+++ doc/device.sql
@@ -28,3 +28,40 @@ create table t_device_exception(
   exceptionData varchar(4000) comment '上报数据内容'
   receiveTime bigint comment '上报时间'
 );
+
+
+drop table if not EXISTS  t_device_operlog;
+create table t_device_operlog(
+  id int PRIMARY  key comment '主键id',
+  deviceId int comment '设备id',
+  operDirective int comment ''
+);
+
+drop table if not EXISTS  t_product;
+create table t_product(
+  id int PRIMARY  key comment '主键id',
+  productKey varchar comment 'PK',
+  productName varchar comment '产品名称',
+  insertTime bigint comment '写入时间',
+  lastUpdateTime bigint comment '最后修改时间'
+);
+
+drop table if not EXISTS t_project ;
+create table t_project(
+  id int PRIMARY  key comment '主键id',
+  projectSn varchar comment '项目序列号',
+  projectName varchar comment '项目名称',
+  insertTime bigint comment '写入时间',
+  lastUpdateTime bigint comment '最后修改时间'
+);
+
+create table if not EXISTS t_device_type(
+id int primary key comment '主键id',
+deviceTypeName varchar(200) comment '设备类型名称',
+functionList varchar(200) comment '功能列表'
+insertTime bigint comment '写入时间'
+)
+
+
+mac deviceTypeId productKey projectKey ps
+
