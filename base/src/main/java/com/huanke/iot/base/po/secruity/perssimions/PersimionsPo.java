997877fc26b041e6196630c5b65e9ff0655cf280
郝仕兢 <haoshijing@corp.netease.com>
2018-04-04 17:19:06 +0800

    逻辑增加

---
+++ base/src/main/java/com/huanke/iot/base/po/secruity/perssimions/PersimionsPo.java
@@ -0,0 +1,11 @@
+package com.huanke.iot.base.po.secruity.perssimions;
+
+import lombok.Data;
+
+/**
+ * @author haoshijing
+ * @version 2018年04月04日 17:18
+ **/
+@Data
+public class PersimionsPo {
+}
---
+++ base/src/main/java/com/huanke/iot/base/po/secruity/user/UserPo.java
@@ -0,0 +1,8 @@
+package com.huanke.iot.base.po.secruity.user;
+
+/**
+ * @author haoshijing
+ * @version 2018年04月04日 17:18
+ **/
+public class UserPo {
+}
