f6600c24b84c10013a714df630e10bd6b0645763
郝仕兢 <haoshijing@corp.netease.com>
2018-04-04 17:48:50 +0800

    设备数据管理

---
+++ base/src/main/java/com/huanke/iot/base/dao/DeviceDao.java
@@ -0,0 +1,11 @@
+package com.huanke.iot.base.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author haoshijing
+ * @version 2018年04月04日 17:47
+ **/
+@Repository
+public class DeviceDao {
+}
