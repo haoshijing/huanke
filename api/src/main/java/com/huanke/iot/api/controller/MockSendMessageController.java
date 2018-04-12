4e1b21631004d5bda4cab483f5046c24980ebfce
郝仕兢 <haoshijing@corp.netease.com>
2018-04-12 13:28:36 +0800

    mock接口实现

--- api/src/main/java/com/huanke/iot/api/controller/MockSendMessageController.java
+++ api/src/main/java/com/huanke/iot/api/controller/MockSendMessageController.java
@@ -6,7 +6,10 @@ import lombok.Data;
 import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -25,9 +28,37 @@ public class MockSendMessageController {
         private List<AlarmMessage> alarm;
     }
 
+    @Data
+    private static class SwitchDataItem{
+        private Integer mode;
+        private Integer devicelock;
+        private Integer childlock;
+        private Integer anion;
+        private Integer uvl;
+        private Integer heater;
+        private List<Item> fan;
+        private List<Item>  valve;
+    }
+    @Data
+    private static class Item{
+        private Integer index;
+        private Integer value;
+    }
+
     @Autowired
     private MqttSendService mqttSendService;
 
+
+    @RequestMapping("/sendControl")
+    @ResponseBody
+    public String sendControl(@RequestBody SwitchDataItem switchDataItem){
+        String topic = "/down/control/1";
+        mqttSendService.sendMessage("/up/alarm/1",JSON.toJSONString(switchDataItem));
+        return "ok";
+    }
+
+
+    @ResponseBody
     @RequestMapping("/sendAlarm")
     public String sendAlarmMessage(){
         AlarmMessage alarmMessage = new AlarmMessage();
