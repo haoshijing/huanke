dacc2cf94a40661154465a6811be23ee13a64efd
郝仕兢 <haoshijing690@163.com>
2018-05-05 16:51:08 +0800

    定时器增加日志

--- job/src/main/java/com/huanke/iot/job/DeviceRemoteJob.java
+++ job/src/main/java/com/huanke/iot/job/DeviceRemoteJob.java
@@ -3,6 +3,7 @@ package com.huanke.iot.job;
 import com.huanke.iot.base.dao.impl.device.DeviceMapper;
 import com.huanke.iot.base.po.device.DevicePo;
 import com.huanke.iot.base.util.LocationUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@Slf4j
 public class DeviceRemoteJob {
     @Autowired
     private DeviceMapper deviceMapper;
@@ -20,6 +22,7 @@ public class DeviceRemoteJob {
 
     @Scheduled(cron = "0 0/2 * * * ?")
     public void doWork(){
+        log.info("start work");
         List<DevicePo> devicePoList = deviceMapper.selectAll();
         devicePoList.forEach(devicePo -> {
             String ip = devicePo.getIp();
@@ -28,5 +31,6 @@ public class DeviceRemoteJob {
                 locationUtils.getWeather(ip,true);
             }
         });
+        log.info(" end work");
     }
 }
