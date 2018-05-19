8a1c50ab8615c108169263d06a9462546d8585ff
郝仕兢 <haoshijing@corp.netease.com>
2018-05-19 14:56:38 +0800

    后台代码开发

---
+++ manage/src/main/java/com/huanke/iot/manage/controller/device/DeviceLogController.java
@@ -0,0 +1,23 @@
+package com.huanke.iot.manage.controller.device;
+
+import com.huanke.iot.base.api.ApiResponse;
+import com.huanke.iot.base.po.device.data.DeviceOperLogPo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author haoshijing
+ * @version 2018年05月19日 14:49
+ **/
+
+@RestController
+@RequestMapping("/device/log")
+public class DeviceLogController {
+
+    @RequestMapping("/queryList")
+    public ApiResponse<DeviceOperLogPo> queryList(){
+        return null;
+    }
+
+}
