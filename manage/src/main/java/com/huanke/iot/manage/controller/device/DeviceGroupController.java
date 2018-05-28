f6d4fa6bdd381543d398bf6adf2d1eb8d67331f1
郝仕兢 <haoshijing690@163.com>
2018-05-29 07:02:34 +0800

    后台设备组增加查询

--- manage/src/main/java/com/huanke/iot/manage/controller/device/DeviceGroupController.java
+++ manage/src/main/java/com/huanke/iot/manage/controller/device/DeviceGroupController.java
@@ -1,30 +1,33 @@
 package com.huanke.iot.manage.controller.device;
 
 import com.huanke.iot.base.api.ApiResponse;
-import com.huanke.iot.manage.controller.device.response.DeviceGroupVo;
-import com.huanke.iot.manage.service.device.DeviceGroupService;
-import org.assertj.core.util.Lists;
+import com.huanke.iot.manage.controller.device.request.DeviceGroupQueryRequest;
+import com.huanke.iot.manage.controller.device.response.DeviceGroupItemVo;
+import com.huanke.iot.manage.service.DeviceGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author haoshijing
- * @version 2018年05月23日 22:35
- **/
+import java.util.List;
+
 @RestController
-@RequestMapping("/manage/group")
+@RequestMapping("/api/deviceGroup")
 public class DeviceGroupController {
 
     @Autowired
     private DeviceGroupService deviceGroupService;
-    public ApiResponse<DeviceGroupVo> selectList(){
-        return new ApiResponse(Lists.newArrayList());
+    @RequestMapping("/select")
+    public ApiResponse<List<DeviceGroupItemVo>> selectList(@RequestBody DeviceGroupQueryRequest request){
 
+        List<DeviceGroupItemVo> groupItemVos = deviceGroupService.selectList(request);
+        return new ApiResponse<>(groupItemVos);
     }
 
-    public ApiResponse<Integer> selectCount(){
-        return new ApiResponse(0);
-    }
+    @RequestMapping("/selectCount")
+    public ApiResponse<Integer> selectCount(@RequestBody DeviceGroupQueryRequest request){
 
+        Integer count = deviceGroupService.selectCount(request);
+        return new ApiResponse<>(count);
+    }
 }
---
+++ manage/src/main/java/com/huanke/iot/manage/service/DeviceGroupService.java
@@ -0,0 +1,39 @@
+package com.huanke.iot.manage.service;
+
+import com.huanke.iot.base.dao.impl.device.DeviceGroupMapper;
+import com.huanke.iot.base.po.device.DeviceGroupPo;
+import com.huanke.iot.manage.controller.device.request.DeviceGroupQueryRequest;
+import com.huanke.iot.manage.controller.device.response.DeviceGroupItemVo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DeviceGroupService {
+
+    @Autowired
+    private DeviceGroupMapper deviceGroupMapper;
+
+    public List<DeviceGroupItemVo> selectList(DeviceGroupQueryRequest request) {
+        DeviceGroupPo deviceGroupPo = new DeviceGroupPo();
+        deviceGroupPo.setGroupName(request.getName());
+        Integer offset = (request.getPage() - 1)*request.getLimit();
+        Integer limit = request.getLimit();
+
+        List<DeviceGroupPo> deviceGroupPos = deviceGroupMapper.selectList(deviceGroupPo,limit,offset);
+
+        return deviceGroupPos.stream().map(deviceGroupPo1 -> {
+            DeviceGroupItemVo itemVo = new DeviceGroupItemVo();
+            itemVo.setGroupName(deviceGroupPo1.getGroupName());
+            itemVo.setId(deviceGroupPo1.getId());
+            itemVo.setIcon(deviceGroupPo1.getIcon());
+            return itemVo;
+        }).collect(Collectors.toList());
+    }
+
+    public Integer selectCount(DeviceGroupQueryRequest request) {
+        DeviceGroupPo deviceGroupPo = new DeviceGroupPo();
+        deviceGroupPo.setGroupName(request.getName());
+        return deviceGroupMapper.selectCount(deviceGroupPo);
+    }
+}
