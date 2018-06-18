eaefebdd19cdee4994bd8d8e337230cfe06a4b0f
haoshijing <haoshijing690@163.com>
2018-06-18 10:01:34 +0800

    cache修改

--- base/src/main/java/com/huanke/iot/base/innercache/PublicNumberCache.java
+++ base/src/main/java/com/huanke/iot/base/innercache/PublicNumberCache.java
@@ -1,11 +1,12 @@
 package com.huanke.iot.base.innercache;
 
 
-import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.huanke.iot.base.dao.impl.publicnumber.PublicNumberMapper;
 import com.huanke.iot.base.po.publicnumber.PublicNumberPo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -13,12 +14,13 @@ import org.springframework.stereotype.Repository;
 import java.util.concurrent.TimeUnit;
 
 @Repository
+@Slf4j
 public class PublicNumberCache {
 
     @Autowired
     private PublicNumberMapper publicNumberMapper;
 
-    private Cache<String, PublicNumberCacheVo> cache = CacheBuilder.newBuilder().expireAfterWrite(2000, TimeUnit.MILLISECONDS).maximumSize(20480).build(new CacheLoader<String, PublicNumberCacheVo>() {
+    private LoadingCache<String, PublicNumberCacheVo> cache = CacheBuilder.newBuilder().expireAfterWrite(2000, TimeUnit.MILLISECONDS).maximumSize(20480).build(new CacheLoader<String, PublicNumberCacheVo>() {
         @Override
         public PublicNumberCacheVo load(String host) throws Exception {
             PublicNumberCacheVo cacheVo = new PublicNumberCacheVo();
@@ -32,6 +34,11 @@ public class PublicNumberCache {
     });
 
     public PublicNumberCacheVo selectByHost(String host){
-        return cache.getIfPresent(host);
+        try {
+            return cache.get(host);
+        }catch (Exception e){
+            log.error("",e);
+            return null;
+        }
     }
 }
