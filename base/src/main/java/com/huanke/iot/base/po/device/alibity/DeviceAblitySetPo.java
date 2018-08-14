472c57f0afd262ce020436b913bcbee5d4495649
caikun <seen_cai@163.com>
2018-08-14 19:50:30 +0800

    增加 设备能力项相关 基础类

---
+++ base/src/main/java/com/huanke/iot/base/po/device/alibity/DeviceAblitySetPo.java
@@ -0,0 +1,15 @@
+package com.huanke.iot.base.po.device.alibity;
+
+/**
+ * @author Caik
+ * @date 2018/8/14 19:19
+ */
+public class DeviceAblitySetPo {
+
+    private Integer id;
+    private String name;
+    private Integer status;
+    private String remark;
+    private Long createTime;
+    private Long lastUpdateTime;
+}
---
+++ base/src/main/java/com/huanke/iot/base/po/device/alibity/DeviceAblitySetRelationPo.java
@@ -0,0 +1,12 @@
+package com.huanke.iot.base.po.device.alibity;
+
+/**
+ * @author Caik
+ * @date 2018/8/14 19:19
+ */
+public class DeviceAblitySetRelationPo {
+
+    private Integer id;
+    private Integer ablityId;
+    private Integer ablitySetId;
+}
