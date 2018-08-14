5a9ff652a172784d463e923932df6110b1b866f1
caikun <seen_cai@163.com>
2018-08-14 22:31:11 +0800

    更改sql编码规则。

--- doc/初始化.sql
+++ doc/初始化.sql
@@ -5,21 +5,21 @@
 DROP TABLE IF EXISTS `t_customer`;
 CREATE TABLE `t_customer`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `loginname` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(200)  DEFAULT NULL,
+  `loginname` varchar(100)  DEFAULT NULL,
   `userType` int(1) NULL DEFAULT NULL,
-  `remark` varchar(2000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `publicName` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `publicId` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `appid` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `appsecret` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `SLD` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `remark` varchar(2000)  DEFAULT NULL,
+  `publicName` varchar(200)  DEFAULT NULL,
+  `publicId` varchar(200)  DEFAULT NULL,
+  `appid` varchar(200)  DEFAULT NULL,
+  `appsecret` varchar(200)  DEFAULT NULL,
+  `SLD` varchar(100)  DEFAULT NULL,
   `status` int(1) NULL DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
-  `creatUser` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `creatUser` varchar(100)  DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '客户表  有公众号的表，B端' ROW_FORMAT = Dynamic;
+)  COMMENT = '客户表  有公众号的表，B端' ;
 
 -- ----------------------------
 -- Table structure for t_customer_user
@@ -27,19 +27,19 @@ CREATE TABLE `t_customer`  (
 DROP TABLE IF EXISTS `t_customer_user`;
 CREATE TABLE `t_customer_user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `openId` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `nickname` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `unionid` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `headimgurl` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `openId` varchar(100)  DEFAULT NULL,
+  `nickname` varchar(200)  DEFAULT NULL,
+  `unionid` varchar(100)  DEFAULT NULL,
+  `headimgurl` varchar(200)  DEFAULT NULL,
   `sex` int(1) NULL DEFAULT NULL,
-  `province` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `city` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `province` varchar(100)  DEFAULT NULL,
+  `city` varchar(100)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   `lastVisitTime` bigint(20) NULL DEFAULT NULL,
-  `mac` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `mac` varchar(100)  DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '终端用户表（C端用户）' ROW_FORMAT = Dynamic;
+)  COMMENT = '终端用户表（C端用户）' ;
 
 -- ----------------------------
 -- Table structure for t_device
@@ -47,24 +47,24 @@ CREATE TABLE `t_customer_user`  (
 DROP TABLE IF EXISTS `t_device`;
 CREATE TABLE `t_device`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mac` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `name` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `deviceId` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `devicelicence` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `saNo` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `mac` varchar(200)  DEFAULT NULL,
+  `name` varchar(200)  DEFAULT NULL,
+  `deviceId` varchar(200)  DEFAULT NULL,
+  `devicelicence` varchar(200)  DEFAULT NULL,
+  `saNo` varchar(200)  DEFAULT NULL,
   `typeId` int(11) NULL DEFAULT NULL,
   `productId` int(11) NULL DEFAULT NULL,
   `onlineStatus` int(1) NULL DEFAULT NULL COMMENT '在线状态',
   `bindStatus` int(1) NULL DEFAULT NULL COMMENT '绑定状态',
   `bindTime` bigint(20) NULL DEFAULT NULL,
-  `ip` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `speedConfig` varchar(4096) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `ip` varchar(200)  DEFAULT NULL,
+  `speedConfig` varchar(4096)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   `enableStatus` int(1) NULL DEFAULT NULL COMMENT '启用状态',
   `workStatus` int(1) NULL DEFAULT NULL COMMENT '工作状态',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备表 （基础属性）' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备表 （基础属性）' ;
 
 -- ----------------------------
 -- Table structure for t_device_ablity
@@ -72,11 +72,11 @@ CREATE TABLE `t_device`  (
 DROP TABLE IF EXISTS `t_device_ablity`;
 CREATE TABLE `t_device_ablity`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `ablityName` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '能力名称',
-  `dirValue` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '通讯对应指令',
+  `ablityName` varchar(1024)  DEFAULT NULL COMMENT '能力名称',
+  `dirValue` varchar(1024)  DEFAULT NULL COMMENT '通讯对应指令',
   `createTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备能力表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备能力表' ;
 
 -- ----------------------------
 -- Table structure for t_device_ablity_option
@@ -84,12 +84,12 @@ CREATE TABLE `t_device_ablity`  (
 DROP TABLE IF EXISTS `t_device_ablity_option`;
 CREATE TABLE `t_device_ablity_option`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `optionName` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `ablityId` varchar(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '通讯对应指令',
+  `optionName` varchar(200)  DEFAULT NULL,
+  `ablityId` varchar(11)  DEFAULT NULL COMMENT '通讯对应指令',
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpaddateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备能力选项表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备能力选项表' ;
 
 -- ----------------------------
 -- Table structure for t_device_ablity_set
@@ -97,13 +97,13 @@ CREATE TABLE `t_device_ablity_option`  (
 DROP TABLE IF EXISTS `t_device_ablity_set`;
 CREATE TABLE `t_device_ablity_set`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
   `status` int(1) NULL DEFAULT NULL,
-  `remark` varchar(500) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `remark` varchar(500)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '功能集' ROW_FORMAT = Dynamic;
+)  COMMENT = '功能集' ;
 
 -- ----------------------------
 -- Table structure for t_device_ablity_set_relation
@@ -114,7 +114,7 @@ CREATE TABLE `t_device_ablity_set_relation`  (
   `ablityId` int(11) NULL DEFAULT NULL,
   `ablitySetId` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '功能集 和能力 关联关系表' ROW_FORMAT = Dynamic;
+)  COMMENT = '功能集 和能力 关联关系表' ;
 
 -- ----------------------------
 -- Table structure for t_device_alarm
@@ -123,7 +123,7 @@ DROP TABLE IF EXISTS `t_device_alarm`;
 CREATE TABLE `t_device_alarm`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备告警信息' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备告警信息' ;
 
 -- ----------------------------
 -- Table structure for t_device_customer_relation
@@ -136,7 +136,7 @@ CREATE TABLE `t_device_customer_relation`  (
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备客户关系表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备客户关系表' ;
 
 -- ----------------------------
 -- Table structure for t_device_customer_user_relation
@@ -151,7 +151,7 @@ CREATE TABLE `t_device_customer_user_relation`  (
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备 和终端用户关系表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备 和终端用户关系表' ;
 
 -- ----------------------------
 -- Table structure for t_device_group
@@ -159,15 +159,15 @@ CREATE TABLE `t_device_customer_user_relation`  (
 DROP TABLE IF EXISTS `t_device_group`;
 CREATE TABLE `t_device_group`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
   `customerId` int(11) NULL DEFAULT NULL,
-  `masterOpenId` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `manageOpenIds` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `masterOpenId` varchar(100)  DEFAULT NULL,
+  `manageOpenIds` varchar(200)  DEFAULT NULL,
   `status` int(11) NULL DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备群' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备群' ;
 
 -- ----------------------------
 -- Table structure for t_device_group_item
@@ -181,7 +181,7 @@ CREATE TABLE `t_device_group_item`  (
   `createTIme` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备群 和设备关系表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备群 和设备关系表' ;
 
 -- ----------------------------
 -- Table structure for t_device_model
@@ -189,18 +189,18 @@ CREATE TABLE `t_device_group_item`  (
 DROP TABLE IF EXISTS `t_device_model`;
 CREATE TABLE `t_device_model`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
   `typeId` int(11) NULL DEFAULT NULL,
   `customerId` int(11) NULL DEFAULT NULL,
   `productId` int(11) NULL DEFAULT NULL,
-  `version` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `icon` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `version` varchar(20)  DEFAULT NULL,
+  `icon` varchar(200)  DEFAULT NULL,
   `status` int(1) NULL DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
-  `remark` varchar(500) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `remark` varchar(500)  DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备型号表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备型号表' ;
 
 -- ----------------------------
 -- Table structure for t_device_model_ablity
@@ -210,12 +210,12 @@ CREATE TABLE `t_device_model_ablity`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `modelId` int(11) NULL DEFAULT NULL,
   `ablityId` int(11) NULL DEFAULT NULL,
-  `definedName` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `definedName` varchar(200)  DEFAULT NULL,
   `status` int(1) NULL DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备型号 功能表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备型号 功能表' ;
 
 -- ----------------------------
 -- Table structure for t_device_model_ablity_option
@@ -224,12 +224,12 @@ DROP TABLE IF EXISTS `t_device_model_ablity_option`;
 CREATE TABLE `t_device_model_ablity_option`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `modelAblityId` int(11) NULL DEFAULT NULL,
-  `definedName` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `definedName` varchar(200)  DEFAULT NULL,
   `status` int(1) NULL DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备型号 功能选项自定义表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备型号 功能选项自定义表' ;
 
 -- ----------------------------
 -- Table structure for t_device_operlog
@@ -246,7 +246,7 @@ CREATE TABLE `t_device_operlog`  (
   `retMsg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '处理结果',
   `createTime` bigint(20) NULL DEFAULT NULL COMMENT '操作时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1328 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1328 CHARACTER SET = utf8 COLLATE = utf8_general_ci ;
 
 -- ----------------------------
 -- Table structure for t_device_team
@@ -254,18 +254,18 @@ CREATE TABLE `t_device_operlog`  (
 DROP TABLE IF EXISTS `t_device_team`;
 CREATE TABLE `t_device_team`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `icon` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `remark` varchar(500) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `masterOpenId` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `icon` varchar(100)  DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
+  `remark` varchar(500)  DEFAULT NULL,
+  `masterOpenId` varchar(100)  DEFAULT NULL,
   `customerId` int(11) NULL DEFAULT NULL COMMENT '客户id',
-  `manageOpenIds` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `manageOpenIds` varchar(200)  DEFAULT NULL,
   `status` int(11) NULL DEFAULT NULL,
-  `createUser` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `createUser` varchar(100)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备组' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备组' ;
 
 -- ----------------------------
 -- Table structure for t_device_team_item
@@ -279,7 +279,7 @@ CREATE TABLE `t_device_team_item`  (
   `createTIme` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备组和设备关系表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备组和设备关系表' ;
 
 -- ----------------------------
 -- Table structure for t_device_type
@@ -287,14 +287,14 @@ CREATE TABLE `t_device_team_item`  (
 DROP TABLE IF EXISTS `t_device_type`;
 CREATE TABLE `t_device_type`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `typeNo` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `icon` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `remark` varchar(500) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
+  `typeNo` varchar(50)  DEFAULT NULL,
+  `icon` varchar(200)  DEFAULT NULL,
+  `remark` varchar(500)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpdateTIme` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备类型 表' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备类型 表' ;
 
 -- ----------------------------
 -- Table structure for t_device_type_ablity_set
@@ -305,7 +305,7 @@ CREATE TABLE `t_device_type_ablity_set`  (
   `typeId` int(11) NULL DEFAULT NULL,
   `ablitySetId` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备类型对应的 功能集表 (1v1)' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备类型对应的 功能集表 (1v1)' ;
 
 -- ----------------------------
 -- Table structure for t_deviceid_pool
@@ -314,12 +314,12 @@ DROP TABLE IF EXISTS `t_deviceid_pool`;
 CREATE TABLE `t_deviceid_pool`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `customerId` int(11) NULL DEFAULT NULL,
-  `deviceId` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `deviceId` varchar(1024)  DEFAULT NULL,
   `status` int(11) NULL DEFAULT NULL,
-  `deviceLicence` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `deviceLicence` varchar(1024)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '设备备案号 池' ROW_FORMAT = Dynamic;
+)  COMMENT = '设备备案号 池' ;
 
 -- ----------------------------
 -- Table structure for t_product
@@ -327,9 +327,9 @@ CREATE TABLE `t_deviceid_pool`  (
 DROP TABLE IF EXISTS `t_product`;
 CREATE TABLE `t_product`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `qrcode` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(100)  DEFAULT NULL,
+  `qrcode` char(10)  DEFAULT NULL,
   `createTime` bigint(20) NULL DEFAULT NULL,
   `lastUpadateTime` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '微信 设备型号备案表' ROW_FORMAT = Dynamic;
+)  COMMENT = '微信 设备型号备案表' ;
