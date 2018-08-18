0d6bef82fc70cfe4bfbdb9a21cf917f3499cb543
caikun <seen_cai@163.com>
2018-08-18 14:30:01 +0800

    设备类型功能集的添加删除修改操作

--- manage/src/main/java/com/huanke/iot/manage/service/device/typeModel/DeviceTypeService.java
+++ manage/src/main/java/com/huanke/iot/manage/service/device/typeModel/DeviceTypeService.java
@@ -1,7 +1,14 @@
 package com.huanke.iot.manage.service.device.typeModel;
 
+import com.huanke.iot.base.api.ApiResponse;
+import com.huanke.iot.base.constant.RetCode;
+import com.huanke.iot.base.dao.device.ablity.DeviceAblitySetMapper;
+import com.huanke.iot.base.dao.device.typeModel.DeviceTypeAblitySetMapper;
 import com.huanke.iot.base.dao.device.typeModel.DeviceTypeMapper;
+import com.huanke.iot.base.po.device.alibity.DeviceAblitySetPo;
+import com.huanke.iot.base.po.device.typeModel.DeviceTypeAblitySetPo;
 import com.huanke.iot.base.po.device.typeModel.DeviceTypePo;
+import com.huanke.iot.manage.vo.request.device.typeModel.DeviceTypeAblitySetCreateOrUpdateRequest;
 import com.huanke.iot.manage.vo.request.device.typeModel.DeviceTypeCreateOrUpdateRequest;
 import com.huanke.iot.manage.vo.request.device.typeModel.DeviceTypeQueryRequest;
 import com.huanke.iot.manage.vo.request.device.typeModel.DeviceTypeCreateOrUpdateRequest;
@@ -15,12 +22,21 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 设备类型 service
+ */
 @Repository
 @Slf4j
 public class DeviceTypeService {
 
     @Autowired
     private DeviceTypeMapper deviceTypeMapper;
+
+    @Autowired
+    private DeviceAblitySetMapper deviceAblitySetMapper;
+
+    @Autowired
+    private DeviceTypeAblitySetMapper deviceTypeAblitySetMapper;
 
     @Value("${accessKeyId}")
     private String accessKeyId;
@@ -34,6 +50,12 @@ public class DeviceTypeService {
     @Value("${bucketName}")
     private String bucketName;
 
+
+    /**
+     * 创建或修改类型
+     * @param typeRequest
+     * @return
+     */
     public Boolean createOrUpdate(DeviceTypeCreateOrUpdateRequest typeRequest) {
 
         int effectCount = 0;
@@ -49,6 +71,82 @@ public class DeviceTypeService {
         return effectCount > 0;
     }
 
+
+
+    /**
+     * 删除 类型  该类型下的型号如何操作？？？？？？？？？？？？？？
+     * @param typeRequest
+     * @return
+     */
+    public Boolean deleteDeviceType(DeviceTypeCreateOrUpdateRequest typeRequest) {
+
+        Boolean ret  =false;
+        //判断当 类型id不为空时
+        if( typeRequest.getId()!=null){
+            //先删除 该 功能
+            ret = deviceTypeMapper.deleteById(typeRequest.getId()) > 0;
+        }else{
+            log.error("类型主键不可为空");
+            return false;
+        }
+        return ret;
+    }
+
+
+    /**
+     * 创建 或修改 类型的功能集
+     * @param request
+     * @return
+     */
+    public ApiResponse<Boolean> createOrUpdateDeviceTypeAblitySet(DeviceTypeAblitySetCreateOrUpdateRequest request) {
+
+        int effectCount = 0;
+        Boolean ret = false;
+        // 主键 不能为空
+        if(request.getAblitySetId()==null||request.getAblitySetId()<=0){
+            return new ApiResponse<>(RetCode.PARAM_ERROR,"该功能集不存在");
+        }
+        if(request.getTypeId()==null||request.getTypeId()<=0){
+            return new ApiResponse<>(RetCode.PARAM_ERROR,"该类型不存在");
+        }
+
+        //保存前 先进行 验证 该类型 和该 功能集是否存在
+        DeviceTypePo queryDeviceTypePo = deviceTypeMapper.selectById(request.getTypeId());
+        DeviceAblitySetPo queryDeviceAblitySetPo = deviceAblitySetMapper.selectById(request.getAblitySetId());
+
+        if(null==queryDeviceTypePo){
+            return new ApiResponse<>(RetCode.PARAM_ERROR,"该类型不存在");
+        }
+
+        if(null==queryDeviceAblitySetPo){
+            return new ApiResponse<>(RetCode.PARAM_ERROR,"该功能集不存在");
+        }
+
+        //只有 类型与功能集都存在才会进行保存
+        DeviceTypeAblitySetPo deviceTypeAblitySetPo = new DeviceTypeAblitySetPo();
+        deviceTypeAblitySetPo.setAblitySetId(request.getAblitySetId());
+        deviceTypeAblitySetPo.setTypeId(request.getTypeId());
+
+        //当主键存在的时候 说明是更新  否则是新增
+        if(request.getId()!=null&&request.getId()>0){
+            deviceTypeAblitySetPo.setId(request.getId());
+            deviceTypeAblitySetPo.setLastUpdateTime(System.currentTimeMillis());
+        }else{
+            deviceTypeAblitySetPo.setCreateTime(System.currentTimeMillis());
+        }
+
+        ret = deviceTypeAblitySetMapper.insert(deviceTypeAblitySetPo)>0;
+
+        return new ApiResponse<>(ret);
+    }
+
+
+
+    /**
+     * 查询类型列表
+     * @param request
+     * @return
+     */
     public List<DeviceTypeVo> selectList(DeviceTypeQueryRequest request) {
 
         DeviceTypePo queryDeviceTypePo = new DeviceTypePo();
@@ -70,6 +168,12 @@ public class DeviceTypeService {
         }).collect(Collectors.toList());
     }
 
+
+    /**
+     * 根据主键查询类型
+     * @param request
+     * @return
+     */
     public DeviceTypeVo selectById(DeviceTypeQueryRequest request) {
 
         DeviceTypePo queryDeviceTypePo = new DeviceTypePo();
