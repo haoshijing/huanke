085d4408830de5f39a76587be2717ecd7f8fd439
caikun <seen_cai@163.com>
2018-08-21 23:55:44 +0800

    修改 设备类型 型号的 请求方式

--- manage/src/main/java/com/huanke/iot/manage/controller/device/typeModel/DeviceModelController.java
+++ manage/src/main/java/com/huanke/iot/manage/controller/device/typeModel/DeviceModelController.java
@@ -6,13 +6,11 @@ import com.huanke.iot.manage.service.device.typeModel.DeviceModelService;
 import com.huanke.iot.manage.vo.request.device.typeModel.DeviceModelCreateOrUpdateRequest;
 import com.huanke.iot.manage.vo.request.device.typeModel.DeviceModelQueryRequest;
 import com.huanke.iot.manage.vo.response.device.typeModel.DeviceModelVo;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +33,7 @@ public class DeviceModelController {
      * @return 成功返回true，失败返回false
      * @throws Exception
      */
+    @ApiOperation("添加新型号")
     @RequestMapping(value = "/createDeviceModel",method = RequestMethod.POST)
     public ApiResponse<Boolean> createDeviceModel(@RequestBody DeviceModelCreateOrUpdateRequest modelRequest) throws Exception{
         if(StringUtils.isBlank(modelRequest.getName()) ){
@@ -54,7 +53,8 @@ public class DeviceModelController {
      * @return 成功返回true，失败返回false
      * @throws Exception
      */
-    @RequestMapping(value = "/updateDeviceModel",method = RequestMethod.POST)
+    @ApiOperation("修改设备型号")
+    @PutMapping(value = "/updateDeviceModel")
     public ApiResponse<Boolean> updateDeviceModel(@RequestBody DeviceModelCreateOrUpdateRequest modelRequest) throws Exception{
         if(modelRequest.getId()==null||modelRequest.getId()<=0){
             return new ApiResponse<>(RetCode.PARAM_ERROR,"型号主键不存在");
@@ -72,7 +72,8 @@ public class DeviceModelController {
      * @return 返回型号项列表
      * @throws Exception
      */
-    @RequestMapping(value = "/select")
+    @ApiOperation("查询设备型号列表")
+    @PostMapping(value = "/select")
     public ApiResponse<List<DeviceModelVo>> selectList(@RequestBody DeviceModelQueryRequest modelRequest) throws Exception{
         List<DeviceModelVo> deviceModelVos =  deviceModelService.selectList(modelRequest);
         return new ApiResponse<>(deviceModelVos);
@@ -84,7 +85,8 @@ public class DeviceModelController {
      * @return 返回型号项列表
      * @throws Exception
      */
-    @RequestMapping(value = "/selectByTypeId")
+    @ApiOperation("根据类型Id 查询型号列表")
+    @GetMapping(value = "/selectByTypeId")
     public ApiResponse<List<DeviceModelVo>> selectByTypeId(@RequestBody DeviceModelQueryRequest modelRequest) throws Exception{
         List<DeviceModelVo> deviceModelVos =  deviceModelService.selectByTypeId(modelRequest);
         return new ApiResponse<>(deviceModelVos);
@@ -96,6 +98,7 @@ public class DeviceModelController {
      * @return 返回型号项列表
      * @throws Exception
      */
+    @ApiOperation("根据主键查询设备型号")
     @RequestMapping(value = "/selectById")
     public ApiResponse<DeviceModelVo> selectById(@RequestBody DeviceModelQueryRequest modelRequest) throws Exception{
         DeviceModelVo deviceModelVo =  deviceModelService.selectById(modelRequest);
