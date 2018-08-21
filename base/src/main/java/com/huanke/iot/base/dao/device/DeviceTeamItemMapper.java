a478584823fc4e74326d25e0c36846ab3bb899c1
caikun <seen_cai@163.com>
2018-08-21 10:26:34 +0800

    增加设备组相关

---
+++ base/src/main/java/com/huanke/iot/base/dao/device/DeviceTeamItemMapper.java
@@ -0,0 +1,19 @@
+package com.huanke.iot.base.dao.device;
+
+import com.huanke.iot.base.dao.BaseMapper;
+import com.huanke.iot.base.po.device.DeviceTeamItemPo;
+
+/**
+ * @author sixiaojun
+ * @version 2018-08-15
+ **/
+public interface DeviceTeamItemMapper extends BaseMapper<DeviceTeamItemPo> {
+
+//    /**
+//     * 根据设备ID查询设备群关系
+//     *sixiaojun
+//     * @param deviceId
+//     * @returnDeviceGroupItemPo
+//     */
+//    DeviceGroupItemPo selectByDeviceId(Integer deviceId);
+}
