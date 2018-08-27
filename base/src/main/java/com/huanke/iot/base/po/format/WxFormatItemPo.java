047b4285cce9c17a10cb8f4cf2d428be1eeb3d66
caikun <seen_cai@163.com>
2018-08-27 22:22:45 +0800

    增加formatId

--- base/src/main/java/com/huanke/iot/base/po/format/WxFormatItemPo.java
+++ base/src/main/java/com/huanke/iot/base/po/format/WxFormatItemPo.java
@@ -9,6 +9,7 @@ import lombok.Data;
 @Data
 public class WxFormatItemPo {
     private Integer id;
+    private Integer formatId;
     private String name;
     private Integer status;
     private Long createTime;
