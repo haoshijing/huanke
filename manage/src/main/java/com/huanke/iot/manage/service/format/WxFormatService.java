f585c7654fe1df6b5f65df5a2998f30ca81eeb0b
caikun <seen_cai@163.com>
2018-08-28 01:29:14 +0800

    修改版式保存方法

--- manage/src/main/java/com/huanke/iot/manage/service/format/WxFormatService.java
+++ manage/src/main/java/com/huanke/iot/manage/service/format/WxFormatService.java
@@ -8,7 +8,9 @@ import com.huanke.iot.base.dao.format.WxFormatItemMapper;
 import com.huanke.iot.base.dao.format.WxFormatMapper;
 import com.huanke.iot.base.po.device.alibity.DeviceAblityOptionPo;
 import com.huanke.iot.base.po.device.alibity.DeviceAblityPo;
+import com.huanke.iot.base.po.format.WxFormatItemPo;
 import com.huanke.iot.base.po.format.WxFormatPo;
+import com.huanke.iot.manage.vo.request.WxFormatQueryRequest;
 import com.huanke.iot.manage.vo.request.device.ablity.DeviceAblityCreateOrUpdateRequest;
 import com.huanke.iot.manage.vo.request.device.ablity.DeviceAblityOptionCreateOrUpdateRequest;
 import com.huanke.iot.manage.vo.request.device.ablity.DeviceAblityQueryRequest;
@@ -35,7 +37,7 @@ public class WxFormatService {
 
 
     /**
-     * 新增 功能
+     * 新增 版式以及版式的 配置项
      *
      * @param wxFormatVo
      * @return
@@ -54,143 +56,124 @@ public class WxFormatService {
             wxFormatPo.setCreateTime(System.currentTimeMillis());
             ret = wxFormatMapper.insert(wxFormatPo) > 0;
         }
-        //判断 该功能里的选项是否为空，若不为空则进行保存
+        //判断 该版式里的配置项是否为空，若不为空则进行保存
         if (wxFormatVo.getWxFormatItemVos() != null && wxFormatVo.getWxFormatItemVos().size() > 0) {
 
-            for (DeviceAblityOptionCreateOrUpdateRequest deviceAblityOptionRequest : ablityRequest.getDeviceAblityOptions()) {
-                DeviceAblityOptionPo deviceAblityOptionPo = new DeviceAblityOptionPo();
-                deviceAblityOptionPo.setOptionName(deviceAblityOptionRequest.getOptionName());
-                deviceAblityOptionPo.setOptionValue(deviceAblityOptionRequest.getOptionValue());
-                deviceAblityOptionPo.setAblityId(deviceAblityPo.getId());
-                deviceAblityOptionPo.setStatus(CommonConstant.STATUS_YES);
-                //如果 该选项有id 则为更新 ，否则为新增
-                if(deviceAblityOptionRequest.getId()!=null&&deviceAblityOptionRequest.getId()>0){
-                    deviceAblityOptionPo.setId(deviceAblityOptionRequest.getId());
-                    deviceAblityOptionPo.setLastUpdateTime(System.currentTimeMillis());
-
-                    if(CommonConstant.STATUS_DEL.equals(deviceAblityOptionRequest.getStatus())){
-                        deviceAblityOptionPo.setStatus(CommonConstant.STATUS_DEL);
+            wxFormatVo.getWxFormatItemVos().stream().forEach(wxFormatItemVo -> {
+                WxFormatItemPo wxFormatItemPo = new WxFormatItemPo();
+                BeanUtils.copyProperties(wxFormatItemVo, wxFormatItemPo);
+                //如果是有id，则该配置项为更新，否则为新增保存
+                if (wxFormatItemVo.getId() != null && wxFormatItemVo.getId() > 0) {
+                    //如果当前 状态 =删除的时候 才会删除，否则 全部为正常状态
+                    if (CommonConstant.STATUS_DEL.equals(wxFormatItemVo.getStatus())) {
+                        wxFormatItemPo.setStatus(CommonConstant.STATUS_DEL);
+                    } else {
+                        wxFormatItemPo.setStatus(CommonConstant.STATUS_YES);
                     }
-                    deviceAblityOptionMapper.updateById(deviceAblityOptionPo);
-                }else{
-
-                    deviceAblityOptionPo.setCreateTime(System.currentTimeMillis());
-                    deviceAblityOptionMapper.insert(deviceAblityOptionPo);
+                    wxFormatItemPo.setLastUpdateTime(System.currentTimeMillis());
+                    wxFormatItemMapper.updateById(wxFormatItemPo);
+                } else {
+                    wxFormatItemPo.setCreateTime(System.currentTimeMillis());
+                    wxFormatItemPo.setStatus(CommonConstant.STATUS_YES);
+                    wxFormatItemMapper.insert(wxFormatItemPo);
                 }
 
+            });
 
-            }
         }
 
-        return new ApiResponse<>(deviceAblityPo.getId());
+        return new ApiResponse<>(wxFormatPo.getId());
     }
 
     /**
-     * 查询功能列表
+     * 查询版式列表
      *
      * @param request
      * @return
      */
-    public List<DeviceAblityVo> selectList(DeviceAblityQueryRequest request) {
+    public List<WxFormatVo> selectList(WxFormatQueryRequest request) {
 
-        DeviceAblityPo queryDeviceAblityPo = new DeviceAblityPo();
-        queryDeviceAblityPo.setAblityName(request.getAblityName());
-        queryDeviceAblityPo.setDirValue(request.getDirValue());
-        queryDeviceAblityPo.setWriteStatus(request.getWriteStatus());
-        queryDeviceAblityPo.setReadStatus(request.getReadStatus());
-        queryDeviceAblityPo.setRunStatus(request.getRunStatus());
-        queryDeviceAblityPo.setConfigType(request.getConfigType());
-        queryDeviceAblityPo.setAblityType(request.getAblityType());
+        WxFormatPo queryWxFormatPo = new WxFormatPo();
 
+        if (request != null) {
+            queryWxFormatPo.setId(request.getId());
+            queryWxFormatPo.setName(request.getName());
+            queryWxFormatPo.setOwerType(request.getOwerType());
+            queryWxFormatPo.setType(request.getType());
+            queryWxFormatPo.setStatus(request.getStatus());
+        }
         Integer offset = (request.getPage() - 1) * request.getLimit();
         Integer limit = request.getLimit();
 
-        //查询 功能列表
-        List<DeviceAblityPo> deviceAblityPos = deviceAblityMapper.selectList(queryDeviceAblityPo, limit, offset);
-        List<DeviceAblityVo> deviceAblityVos = deviceAblityPos.stream().map(deviceAblityPo -> {
-            DeviceAblityVo deviceAblityVo = new DeviceAblityVo();
-            deviceAblityVo.setAblityName(deviceAblityPo.getAblityName());
-            deviceAblityVo.setDirValue(deviceAblityPo.getDirValue());
-            deviceAblityVo.setWriteStatus(deviceAblityPo.getWriteStatus());
-            deviceAblityVo.setReadStatus(deviceAblityPo.getReadStatus());
-            deviceAblityVo.setRunStatus(deviceAblityPo.getRunStatus());
-            deviceAblityVo.setConfigType(deviceAblityPo.getConfigType());
-            deviceAblityVo.setAblityType(deviceAblityPo.getAblityType());
-            deviceAblityVo.setRemark(deviceAblityPo.getRemark());
-            deviceAblityVo.setId(deviceAblityPo.getId());
+        //查询 版式列表
+        List<WxFormatPo> wxformatPoList = wxFormatMapper.selectList(queryWxFormatPo, limit, offset);
+        List<WxFormatVo> wxFormatVoList = wxformatPoList.stream().map(wxFormatPo -> {
+            WxFormatVo wxFormatVo = new WxFormatVo();
+            BeanUtils.copyProperties(wxFormatPo, wxFormatVo);
 
-            //根据功能主键 查询该功能下的 选项列表
-            List<DeviceAblityOptionPo> deviceAblityOptionpos = deviceAblityOptionMapper.selectOptionsByAblityId(deviceAblityPo.getId());
-            List<DeviceAblityOptionVo> deviceAblityOptionVos = deviceAblityOptionpos.stream().map(deviceAblityOptionPo -> {
-                DeviceAblityOptionVo deviceAblityOptionVo = new DeviceAblityOptionVo();
-                deviceAblityOptionVo.setOptionValue(deviceAblityOptionPo.getOptionValue());
-                deviceAblityOptionVo.setOptionName(deviceAblityOptionPo.getOptionName());
-                deviceAblityOptionVo.setId(deviceAblityOptionPo.getId());
-                deviceAblityOptionVo.setStatus(deviceAblityOptionPo.getStatus());
-                return deviceAblityOptionVo;
-            }).collect(Collectors.toList());
 
-            deviceAblityVo.setDeviceAblityOptions(deviceAblityOptionVos);
-            return deviceAblityVo;
+
+            return wxFormatVo;
         }).collect(Collectors.toList());
 
-        return deviceAblityVos;
+        return wxFormatVoList;
     }
 
-
-    /**
-     * 根据主键查询 功能
-     *
-     * @param typeId
-     * @return
-     */
-    public DeviceAblityVo selectById(Integer typeId) {
-
-        DeviceAblityPo deviceAblityPo = deviceAblityMapper.selectById(typeId);
-
-        DeviceAblityVo deviceAblityVo = new DeviceAblityVo();
-        if(deviceAblityPo!=null){
-            deviceAblityVo.setAblityName(deviceAblityPo.getAblityName());
-            deviceAblityVo.setDirValue(deviceAblityPo.getDirValue());
-            deviceAblityVo.setWriteStatus(deviceAblityPo.getWriteStatus());
-            deviceAblityVo.setReadStatus(deviceAblityPo.getReadStatus());
-            deviceAblityVo.setRunStatus(deviceAblityPo.getRunStatus());
-            deviceAblityVo.setConfigType(deviceAblityPo.getConfigType());
-            deviceAblityVo.setAblityType(deviceAblityPo.getAblityType());
-            deviceAblityVo.setRemark(deviceAblityPo.getRemark());
-            deviceAblityVo.setId(deviceAblityPo.getId());
-
-            //根据功能主键 查询该功能下的 选项列表
-            List<DeviceAblityOptionPo> deviceAblityOptionpos = deviceAblityOptionMapper.selectOptionsByAblityId(deviceAblityPo.getId());
-            List<DeviceAblityOptionVo> deviceAblityOptionVos = deviceAblityOptionpos.stream().map(deviceAblityOptionPo -> {
-                DeviceAblityOptionVo deviceAblityOptionVo = new DeviceAblityOptionVo();
-                deviceAblityOptionVo.setOptionValue(deviceAblityOptionPo.getOptionValue());
-                deviceAblityOptionVo.setOptionName(deviceAblityOptionPo.getOptionName());
-                deviceAblityOptionVo.setId(deviceAblityOptionPo.getId());
-                deviceAblityOptionVo.setStatus(deviceAblityOptionPo.getStatus());
-                return deviceAblityOptionVo;
-            }).collect(Collectors.toList());
-
-            deviceAblityVo.setDeviceAblityOptions(deviceAblityOptionVos);
-        }
-        return deviceAblityVo;
-    }
-    /**
-     * 删除 该功能
-     * 并同时删除该功能下 所有的选项
-     *
-     * @param ablityId
-     * @return
-     */
-    public Boolean deleteAblity(Integer ablityId) {
-
-        Boolean ret = false;
-        //先删除 该 功能
-        ret = deviceAblityMapper.deleteById(ablityId) > 0;
-        //再删除 选项表中 的选项
-        ret = ret && deviceAblityMapper.deleteOptionByAblityId(ablityId) > 0;
-        return ret;
-    }
+//
+//    /**
+//     * 根据主键查询 功能
+//     *
+//     * @param typeId
+//     * @return
+//     */
+//    public DeviceAblityVo selectById(Integer typeId) {
+//
+//        DeviceAblityPo deviceAblityPo = deviceAblityMapper.selectById(typeId);
+//
+//        DeviceAblityVo deviceAblityVo = new DeviceAblityVo();
+//        if (deviceAblityPo != null) {
+//            deviceAblityVo.setAblityName(deviceAblityPo.getAblityName());
+//            deviceAblityVo.setDirValue(deviceAblityPo.getDirValue());
+//            deviceAblityVo.setWriteStatus(deviceAblityPo.getWriteStatus());
+//            deviceAblityVo.setReadStatus(deviceAblityPo.getReadStatus());
+//            deviceAblityVo.setRunStatus(deviceAblityPo.getRunStatus());
+//            deviceAblityVo.setConfigType(deviceAblityPo.getConfigType());
+//            deviceAblityVo.setAblityType(deviceAblityPo.getAblityType());
+//            deviceAblityVo.setRemark(deviceAblityPo.getRemark());
+//            deviceAblityVo.setId(deviceAblityPo.getId());
+//
+//            //根据功能主键 查询该功能下的 选项列表
+//            List<DeviceAblityOptionPo> deviceAblityOptionpos = deviceAblityOptionMapper.selectOptionsByAblityId(deviceAblityPo.getId());
+//            List<DeviceAblityOptionVo> deviceAblityOptionVos = deviceAblityOptionpos.stream().map(deviceAblityOptionPo -> {
+//                DeviceAblityOptionVo deviceAblityOptionVo = new DeviceAblityOptionVo();
+//                deviceAblityOptionVo.setOptionValue(deviceAblityOptionPo.getOptionValue());
+//                deviceAblityOptionVo.setOptionName(deviceAblityOptionPo.getOptionName());
+//                deviceAblityOptionVo.setId(deviceAblityOptionPo.getId());
+//                deviceAblityOptionVo.setStatus(deviceAblityOptionPo.getStatus());
+//                return deviceAblityOptionVo;
+//            }).collect(Collectors.toList());
+//
+//            deviceAblityVo.setDeviceAblityOptions(deviceAblityOptionVos);
+//        }
+//        return deviceAblityVo;
+//    }
+//
+//    /**
+//     * 删除 该功能
+//     * 并同时删除该功能下 所有的选项
+//     *
+//     * @param ablityId
+//     * @return
+//     */
+//    public Boolean deleteAblity(Integer ablityId) {
+//
+//        Boolean ret = false;
+//        //先删除 该 功能
+//        ret = deviceAblityMapper.deleteById(ablityId) > 0;
+//        //再删除 选项表中 的选项
+//        ret = ret && deviceAblityMapper.deleteOptionByAblityId(ablityId) > 0;
+//        return ret;
+//    }
 
 
 //    public Integer selectCount(DeviceTypeQueryRequest queryRequest) {
---
+++ manage/src/main/java/com/huanke/iot/manage/vo/request/WxFormatQueryRequest.java
@@ -0,0 +1,25 @@
+package com.huanke.iot.manage.vo.request;
+
+import lombok.Data;
+
+/**
+ * @author caikun
+ * @date 2018/8/28 上午1:16
+ **/
+@Data
+public class WxFormatQueryRequest {
+    private Integer id;
+    private String name;
+//    private String htmlUrl;
+//    private String icon;
+//    private String previewImg;
+//    private String typeIds;
+//    private String customerIds;
+//    private String version;
+    private Integer type;
+    private Integer owerType;
+    private Integer status;
+
+    private Integer page = 1;
+    private Integer limit = 20;
+}
