fc4fc1ca2442291078dd9bcf99bc7244b96b8b3d
Strawberry157 <997008397@qq.com>
2018-08-28 14:55:02 +0800

    h5添加版式

---
+++ api/src/main/java/com/huanke/iot/api/controller/h5/DeviceFormatController.java
@@ -0,0 +1,29 @@
+package com.huanke.iot.api.controller.h5;
+
+import com.huanke.iot.api.controller.h5.response.DeviceDetailVo;
+import com.huanke.iot.api.controller.h5.response.DeviceListVo;
+import com.huanke.iot.api.service.device.basic.DeviceDataService;
+import com.huanke.iot.base.api.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author onlymark
+ * @version 2018年08月28日
+ **/
+@RequestMapping("/h5/api/format")
+@RestController
+public class DeviceFormatController {
+    @Autowired
+    private DeviceDataService deviceDataService;
+    /**
+     * 设备id查版式
+     * @return
+     */
+    @RequestMapping("/obtainByDeviceId")
+    public ApiResponse<DeviceListVo> obtainMyDevice(String deviceId) {
+        DeviceDetailVo deviceDetailVo = deviceDataService.queryDetailByDeviceId(deviceId);
+        return new ApiResponse<>(deviceListVo);
+    }
+}
---
+++ api/src/main/java/com/huanke/iot/api/controller/h5/response/DeviceFormatVo.java
@@ -0,0 +1,12 @@
+package com.huanke.iot.api.controller.h5.response;
+
+import lombok.Data;
+
+/**
+ * @author onlymark
+ * @version 2018年08月28日
+ **/
+@Data
+public class DeviceFormatVo {
+
+}
