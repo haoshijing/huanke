0d37757de2a41b3695ccd49b22afada9c5eab799
Strawberry157 <997008397@qq.com>
2018-08-30 15:27:35 +0800

    新硬件需求：简化json传输

--- gateway/src/main/java/com/huanke/iot/gateway/io/impl/SensorHandler.java
+++ gateway/src/main/java/com/huanke/iot/gateway/io/impl/SensorHandler.java
@@ -6,6 +6,7 @@ import com.huanke.iot.base.po.device.data.DeviceSensorPo;
 import com.huanke.iot.gateway.io.AbstractHandler;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -25,6 +26,9 @@ public class SensorHandler  extends AbstractHandler {
     public static class SensorMessage{
         private String type;
         private Integer value;
+        //根据硬件要求，重新设定type，value
+        private String t;
+        private Integer v;
     }
 
     @Data
@@ -46,13 +50,21 @@ public class SensorHandler  extends AbstractHandler {
         sensorListMessage.getDatas().forEach(sensorMessage -> {
             Integer deviceId = getDeviceIdFromTopic(topic);
             DeviceSensorPo deviceSensorPo = new DeviceSensorPo();
-            deviceSensorPo.setSensorType(sensorMessage.getType());
-            deviceSensorPo.setSensorValue(sensorMessage.getValue());
+            String type = sensorMessage.getType();
+            if(StringUtils.isBlank(type)){
+                type = sensorMessage.getT();
+            }
+            deviceSensorPo.setSensorType(type);
+            Integer value = sensorMessage.getValue();
+            if(value == null){
+                value = sensorMessage.getV();
+            }
+            deviceSensorPo.setSensorValue(value);
             deviceSensorPo.setCreateTime(System.currentTimeMillis());
             deviceSensorPo.setDeviceId(getDeviceIdFromTopic(topic));
             try {
                 deviceSensorDataMapper.insert(deviceSensorPo);
-                stringRedisTemplate.opsForHash().put("sensor." + deviceId, sensorMessage.getType(), String.valueOf(sensorMessage.getValue()));
+                stringRedisTemplate.opsForHash().put("sensor." + deviceId, type, String.valueOf(value));
             }catch (Exception e){
                 log.error("",e);
             }
