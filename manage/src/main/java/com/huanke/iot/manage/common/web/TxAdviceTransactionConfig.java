72a87ef0e509a42525599e95b402e42a57a173b8
caikun <seen_cai@163.com>
2018-09-11 19:46:15 +0800

    增加 事务管理

---
+++ manage/src/main/java/com/huanke/iot/manage/common/web/TxAdviceTransactionConfig.java
@@ -0,0 +1,64 @@
+package com.huanke.iot.manage.common.web;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.interceptor.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 注解声明式事务配置
+ */
+@Configuration
+public class TxAdviceTransactionConfig {
+    private static final String AOP_POINTCUT_EXPRESSION = "execution (* com.huanke.iot.manage.service..*.*(..))";
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public TransactionInterceptor txAdvice() {
+        NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+
+        //只读事务，不做更新操作
+        RuleBasedTransactionAttribute readOnlyTx = new RuleBasedTransactionAttribute();
+        readOnlyTx.setReadOnly(true);
+        readOnlyTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_NOT_SUPPORTED);
+
+        //当前存在事务就使用当前事务，当前不存在事务就创建一个新的事务
+        RuleBasedTransactionAttribute requiredTx = new RuleBasedTransactionAttribute();
+        requiredTx.setRollbackRules(Collections.singletonList(new RollbackRuleAttribute(Exception.class)));
+        requiredTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        requiredTx.setTimeout(10000);
+
+
+        Map<String, TransactionAttribute> txMap = new HashMap<String, TransactionAttribute>();
+        txMap.put("query*", readOnlyTx);
+        txMap.put("get*", readOnlyTx);
+        txMap.put("check*", readOnlyTx);
+        txMap.put("select*", readOnlyTx);
+        txMap.put("obtain*", readOnlyTx);
+
+        txMap.put("*", requiredTx);
+        txMap.put("create*", requiredTx);
+        source.setNameMap(txMap);
+
+        TransactionInterceptor txAdvice = new TransactionInterceptor(transactionManager, source);
+        return txAdvice;
+    }
+
+    @Bean
+    public Advisor txAdviceAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(AOP_POINTCUT_EXPRESSION);
+        return new DefaultPointcutAdvisor(pointcut, txAdvice());
+    }
+}
