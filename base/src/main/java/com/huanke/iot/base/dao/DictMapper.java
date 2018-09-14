9d5c46ca30c6f921adac8885cb885566be959b37
Strawberry157 <997008397@qq.com>
2018-09-14 14:05:05 +0800

    提交字典表基本类

---
+++ base/src/main/java/com/huanke/iot/base/dao/DictMapper.java
@@ -0,0 +1,12 @@
+package com.huanke.iot.base.dao;
+
+import com.huanke.iot.base.po.config.DictPo;
+
+/**
+ * 描述:
+ *
+ * @author onlymark
+ * @create 2018-09-14 下午1:54
+ */
+public interface DictMapper extends BaseMapper<DictPo> {
+}
---
+++ base/src/main/java/com/huanke/iot/base/po/config/DictPo.java
@@ -0,0 +1,30 @@
+package com.huanke.iot.base.po.config;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author onlymark
+ * @version 2018年09月13日 09:22
+ **/
+
+@Data
+public class DictPo {
+    /**
+     * id
+     */
+    private Integer id;
+    private Integer value;
+    private String label;
+    private String type;
+    private String description;
+    private Integer sort;
+    private String remarks;
+    private Integer operationUserId;
+
+    private Date createTime;
+    private Date updateTime;
+    private Integer isDelete;
+
+}
