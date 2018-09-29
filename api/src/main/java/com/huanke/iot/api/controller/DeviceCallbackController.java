dc221d1cbf0c2d6b442fbc3ce9a7cfb851f8ae28
caikun <seen_cai@163.com>
2018-09-29 18:08:00 +0800

    增加 关注公众号 新增用户

--- api/src/main/java/com/huanke/iot/api/controller/DeviceCallbackController.java
+++ api/src/main/java/com/huanke/iot/api/controller/DeviceCallbackController.java
@@ -1,6 +1,7 @@
 package com.huanke.iot.api.controller;
 
 import com.huanke.iot.api.service.device.basic.DeviceBindService;
+import com.huanke.iot.api.service.user.UserService;
 import com.huanke.iot.api.util.MessageUtil;
 import com.huanke.iot.api.util.SignUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +24,16 @@ import java.util.Map;
 public class DeviceCallbackController {
     @Autowired
     DeviceBindService deviceBindService;
+
+    @Autowired
+    UserService userService;
+
     @ResponseBody
     @RequestMapping("/onEvent")
-    public String  checkCode(HttpServletRequest request){
+    public String checkCode(HttpServletRequest request) {
         try {
             String method = request.getMethod();
-            if(StringUtils.equalsIgnoreCase(method,"get")) {
+            if (StringUtils.equalsIgnoreCase(method, "get")) {
                 String signature = request.getParameter("signature");
                 String timestamp = request.getParameter("timestamp");
                 String nonce = request.getParameter("nonce");
@@ -37,12 +42,12 @@ public class DeviceCallbackController {
                 if (checkOk) {
                     return echostr;
                 }
-            }else if(StringUtils.equalsIgnoreCase("post",method)){
+            } else if (StringUtils.equalsIgnoreCase("post", method)) {
                 return processPostRequest(request);
             }
 
-        }catch (Exception e){
-            log.error("",e);
+        } catch (Exception e) {
+            log.error("", e);
         }
         return "";
     }
@@ -53,17 +58,25 @@ public class DeviceCallbackController {
         String respContent = "请求处理异常，请稍后尝试！";
         try {
             //xml请求解析
-            Map<String,String> requestMap = MessageUtil.pareXml(request);
+            Map<String, String> requestMap = MessageUtil.pareXml(request);
             String msgType = requestMap.get("MsgType");
             // reqMap = {DeviceType=gh_7f3ba47c70a3, DeviceID=gh_7f3ba47c70a3_f1c1cd2015ab27b6, Con
-           // tent=, CreateTime=1523200569, Event=unbind, ToUserName=gh_7f3ba47c70a3, FromUserName=okOTjwpDwxJR666hVWnj_L_jp87w, MsgType=device_event, SessionID=0, OpenID=okOTjwpDwxJR666hVWnj_L_jp87w}
+            // tent=, CreateTime=1523200569, Event=unbind, ToUserName=gh_7f3ba47c70a3, FromUserName=okOTjwpDwxJR666hVWnj_L_jp87w, MsgType=device_event, SessionID=0, OpenID=okOTjwpDwxJR666hVWnj_L_jp87w}
 
-        if(StringUtils.equals(msgType,"device_event")){
-            String event = requestMap.get("Event");
-            deviceBindService.handlerDeviceEvent(request,requestMap,event);
-        }
+            if (StringUtils.equals(msgType, "device_event")) {
+                String event = requestMap.get("Event");
+                deviceBindService.handlerDeviceEvent(request, requestMap, event);
+            }
+//            else if (StringUtils.equals(msgType, "event")) {
+////                reqMap = {CreateTime=1538207654, EventKey=, Event=subscribe, ToUserName=gh_f5f0fdaff947,
+//// FromUserName=oOSkz0XQn4q-6HWEpfeNRaSbkhY4, MsgType=event}
+//
+//                String event = requestMap.get("Event");
+//
+//                userService.attentionPublicNumber(request, requestMap, event);
+//            }
         } catch (Exception e) {
-            log.error("",e);
+            log.error("", e);
         }
         return respMessage;
     }
