cb9d5c5d2264edbd846161fc6c9e8f084c45b189
zdy <965082472@qq.com>
2018-10-16 19:48:28 +0800

    修改APP接口

--- api/src/main/java/com/huanke/iot/api/service/device/basic/AppBasicService.java
+++ api/src/main/java/com/huanke/iot/api/service/device/basic/AppBasicService.java
@@ -1,13 +1,18 @@
 package com.huanke.iot.api.service.device.basic;
 
 import com.alibaba.fastjson.JSONObject;
+import com.huanke.iot.api.requestcontext.UserRequestContext;
+import com.huanke.iot.api.requestcontext.UserRequestContextHolder;
 import com.huanke.iot.api.wechat.WechartUtil;
+import com.huanke.iot.base.dao.customer.CustomerMapper;
 import com.huanke.iot.base.dao.customer.CustomerUserMapper;
+import com.huanke.iot.base.po.customer.CustomerPo;
 import com.huanke.iot.base.po.customer.CustomerUserPo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,10 +25,26 @@ public class AppBasicService {
 
     @Autowired
     private CustomerUserMapper customerUserMapper;
+    @Autowired
+    private CustomerMapper customerMapper;
 
     @Transactional
     public boolean addUserAppInfo(HttpServletRequest request){
+        String appId = request.getParameter("appId");
+        CustomerPo customerPo = customerMapper.selectByAppId(appId);
+        if(customerPo == null){
+            return false;
+        }
+        UserRequestContext context = UserRequestContextHolder.get();
+        if(context.getCustomerVo() == null){
+            context.setCustomerVo(new UserRequestContext.CustomerVo());
+        }
+        context.getCustomerVo().setAppId(appId);
+        context.getCustomerVo().setCustomerId(customerPo.getId());
         JSONObject resp = wechartUtil.obtainAuthAccessToken(request.getParameter("code"));
+        if(resp == null || StringUtils.isEmpty(resp.get("openId"))){
+            return false;
+        }
         String iMei = request.getParameter("iMei");
         String openId = resp.getString("openId");
         CustomerUserPo customerUserPo = customerUserMapper.selectByMac(iMei);
