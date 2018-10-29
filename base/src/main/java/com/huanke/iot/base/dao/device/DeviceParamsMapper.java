ea76f577aa6b219c61bd349576cbab8064506e4b
Strawberry157 <997008397@qq.com>
2018-10-29 14:01:15 +0800

    添加设备传参类

---
+++ base/src/main/java/com/huanke/iot/base/dao/device/DeviceParamsMapper.java
@@ -0,0 +1,7 @@
+package com.huanke.iot.base.dao.device;
+
+import com.huanke.iot.base.dao.BaseMapper;
+import com.huanke.iot.base.po.device.DeviceParamsPo;
+
+public interface DeviceParamsMapper extends BaseMapper<DeviceParamsPo> {
+}
