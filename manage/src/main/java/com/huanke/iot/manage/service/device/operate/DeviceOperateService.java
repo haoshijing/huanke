23e142af445210dc45516964e795638379f2482e
sixiaojun <driftingjane@163.com>
2018-10-29 22:22:02 +0800

    设备开关机数据优化

--- manage/src/main/java/com/huanke/iot/manage/service/device/operate/DeviceOperateService.java
+++ manage/src/main/java/com/huanke/iot/manage/service/device/operate/DeviceOperateService.java
@@ -327,8 +327,8 @@ public class DeviceOperateService {
             DeviceOperLogPo deviceOperLogPo = this.deviceOperLogMapper.queryPowerByCreateTime(devicePo.getId());
             if(null != deviceOperLogPo){
                 //0-关机 1-开机
-                log.info("设备的开机关机状态:",deviceOperLogPo);
-                deviceQueryVo.setPowerStatus(deviceOperLogPo.getFuncValue().equals("0")?0:1);
+                log.info("设备的开机关机状态:",deviceOperLogPo.getFuncValue());
+                deviceQueryVo.setPowerStatus(deviceOperLogPo.getFuncValue().equals("0") ? 0:1);
             }else {
                 deviceQueryVo.setPowerStatus(0);
             }
