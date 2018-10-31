c94382e05be7ec81532fbc13d08a6bf4b16473b1
sixiaojun <driftingjane@163.com>
2018-10-31 09:38:58 +0800

    日志请求方式

--- manage/src/main/java/com/huanke/iot/manage/controller/device/operate/DeviceDataController.java
+++ manage/src/main/java/com/huanke/iot/manage/controller/device/operate/DeviceDataController.java
@@ -24,7 +24,7 @@ public class DeviceDataController {
     private DeviceDataService deviceDataService;
 
     @ApiOperation("查看操作日志")
-    @RequestMapping(value = "/queryOperLog",method = RequestMethod.POST)
+    @GetMapping(value = "/queryOperLog")
     public ApiResponse<BaseListVo> queryOperLog(@RequestBody DeviceDataQueryRequest deviceDataQueryRequest){
         try {
             return this.deviceDataService.queryOperLogList(deviceDataQueryRequest);
@@ -34,7 +34,7 @@ public class DeviceDataController {
         }
     }
     @ApiOperation("查看设备数据")
-    @RequestMapping(value = "/queryDeviceSensorStat",method = RequestMethod.POST)
+    @GetMapping(value = "/queryDeviceSensorStat")
     public ApiResponse<BaseListVo> queryDeviceSensorStat(@RequestBody DeviceDataQueryRequest deviceDataQueryRequest){
         try {
             return this.deviceDataService.queryDeivceSensorDataList(deviceDataQueryRequest);
@@ -45,7 +45,7 @@ public class DeviceDataController {
     }
 
     @ApiOperation("查看工作日志")
-    @RequestMapping(value = "/queryDeviceWorkLog",method = RequestMethod.POST)
+    @GetMapping(value = "/queryDeviceWorkLog")
     public ApiResponse<BaseListVo> queryDeviceWorkLog(@RequestBody DeviceDataQueryRequest deviceDataQueryRequest){
         try {
             return this.deviceDataService.queryDeviceWorkDataList(deviceDataQueryRequest);
