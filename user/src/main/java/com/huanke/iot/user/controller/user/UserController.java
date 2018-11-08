6e6301670b8b747f40db0d4949d97965d8488dcf
caikun <seen_cai@163.com>
2018-11-08 16:08:16 +0800

    修改 用户查询接口名称

--- user/src/main/java/com/huanke/iot/user/controller/user/UserController.java
+++ user/src/main/java/com/huanke/iot/user/controller/user/UserController.java
@@ -100,7 +100,7 @@ public class UserController {
 
     @RequiresAuthentication
     @ApiOperation("根据id获取用户详情")
-    @GetMapping("/getUserList/{id}")
+    @GetMapping("/getUserById/{id}")
     public ApiResponse<User> getUserById(@PathVariable("id") Integer userId) {
 
         return new ApiResponse<>(userService.getUserById(userId));
