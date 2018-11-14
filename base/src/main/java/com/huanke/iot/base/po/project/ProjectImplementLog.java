9a34a4176d872094eff5799b7c7c01602bce924f
Strawberry157 <997008397@qq.com>
2018-11-14 09:57:08 +0800

    添加工程管理实体类

---
+++ base/src/main/java/com/huanke/iot/base/po/project/ProjectImplementLog.java
@@ -0,0 +1,27 @@
+package com.huanke.iot.base.po.project;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 描述:
+ * 工程实施记录表
+ *
+ * @author onlymark
+ * @create 2018-11-14 上午9:22
+ */
+@Data
+public class ProjectImplementLog {
+    private Integer id;
+    private String projectNo;
+    private Integer typeId;
+    private String description;
+    private Date implTime;
+    private String imgList;//图册
+    private String fileList;//文件json
+    private Date createTime;
+    private Date updateTime;
+    private Integer createUser;
+    private Integer updateUser;
+}
---
+++ base/src/main/java/com/huanke/iot/base/po/project/ProjectJobInfo.java
@@ -0,0 +1,34 @@
+package com.huanke.iot.base.po.project;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 描述:
+ * 任务表
+ *
+ * @author onlymark
+ * @create 2018-11-14 上午9:26
+ */
+@Data
+public class ProjectJobInfo {
+    private Integer id;
+    private Integer type;
+    private Integer linkDeviceId;//关联设备id
+    private Integer linkProjectId;//关联工程id
+    private String name;
+    private String description;
+    private Integer isRule;
+    private Integer warnLevel;
+    private Integer sourceType;//来源类型：1-计划维保；2-H5端反馈；3-设备告警
+    private Date finalTime;
+    private String imgList;//图册
+    private String enableUsers;
+    private Integer warnStatus;//1-正常；2-告警
+    private Integer flowStatus;//1-待处理；2-处理中；3-待审核；4-已完成；5-已忽略
+    private Date createTime;
+    private Date updateTime;
+    private Integer createUser;
+    private Integer updateUser;
+}
