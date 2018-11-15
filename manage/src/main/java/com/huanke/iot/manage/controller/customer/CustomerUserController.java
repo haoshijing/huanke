ec32e09ef406d11a9ceb769b86ea712343330727
sixiaojun <driftingjane@163.com>
2018-11-15 17:06:47 +0800

    用户列表接口

--- manage/src/main/java/com/huanke/iot/manage/controller/customer/CustomerUserController.java
+++ manage/src/main/java/com/huanke/iot/manage/controller/customer/CustomerUserController.java
@@ -1,14 +1,15 @@
 package com.huanke.iot.manage.controller.customer;
 
 import com.huanke.iot.base.api.ApiResponse;
+import com.huanke.iot.base.constant.RetCode;
 import com.huanke.iot.manage.service.customer.CustomerUserService;
+import com.huanke.iot.manage.vo.request.customer.CustomerUserQueryRequest;
+import com.huanke.iot.manage.vo.response.device.BaseListVo;
 import com.huanke.iot.manage.vo.response.device.customer.CustomerUserVo;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +21,17 @@ import java.util.List;
 @RequestMapping("/api/customerUser")
 @Slf4j
 public class CustomerUserController {
+    @Autowired
+    private CustomerUserService customerUserService;
 
-
-
+    @ApiOperation("查询用户列表及总数")
+    @PostMapping(value = "/selectCustomerUser")
+    public ApiResponse<BaseListVo> selectCustomerUser(@RequestBody CustomerUserQueryRequest customerUserQueryRequest){
+        try{
+            return this.customerUserService.queryCustomerUserList(customerUserQueryRequest);
+        }catch (Exception e){
+            log.error("查询用户列表及总数异常：{}",e);
+            return new ApiResponse<>(RetCode.ERROR,"查询用户列表及总数失败");
+        }
+    }
 }
--- manage/src/main/java/com/huanke/iot/manage/service/customer/CustomerUserService.java
+++ manage/src/main/java/com/huanke/iot/manage/service/customer/CustomerUserService.java
@@ -4,7 +4,6 @@ package com.huanke.iot.manage.service.customer;
 import com.huanke.iot.base.api.ApiResponse;
 import com.huanke.iot.base.constant.RetCode;
 import com.huanke.iot.base.dao.customer.CustomerUserMapper;
-import com.huanke.iot.base.dao.device.DeviceCustomerUserRelationMapper;
 import com.huanke.iot.base.po.customer.CustomerUserPo;
 import com.huanke.iot.manage.vo.request.customer.CustomerUserQueryRequest;
 import com.huanke.iot.manage.vo.response.device.BaseListVo;
@@ -27,11 +26,20 @@ public class CustomerUserService {
     private CustomerUserMapper customerUserMapper;
     @Autowired
     private CustomerService customerService;
-    @Autowired
-    private DeviceCustomerUserRelationMapper deviceCustomerUserRelationMapper;
+
+    public ApiResponse<BaseListVo> queryCustomerUserList(CustomerUserQueryRequest customerUserQueryRequest){
+        BaseListVo baseListVo = new BaseListVo();
+        List<CustomerUserListVo> customerUserListVoList =queryCustomerUser(customerUserQueryRequest);
+        if(null == customerUserListVoList || 0 == customerUserListVoList.size()){
+            return new ApiResponse<>(RetCode.OK,"暂无数据");
+        }
+        Integer totalCount = queryCount(customerUserQueryRequest);
+        baseListVo.setDataList(customerUserListVoList);
+        baseListVo.setTotalCount(totalCount);
+        return new ApiResponse<>(RetCode.OK,"查询用户列表及总数成功",baseListVo);
+    }
 
     public List<CustomerUserListVo> queryCustomerUser(CustomerUserQueryRequest customerUserQueryRequest){
-        BaseListVo baseListVo = new BaseListVo();
         Integer offset = (customerUserQueryRequest.getPage() - 1) * customerUserQueryRequest.getLimit();
         Integer limit = customerUserQueryRequest.getLimit();
         Integer customerId = customerService.obtainCustomerId(false);
@@ -52,5 +60,17 @@ public class CustomerUserService {
             return customerUserListVo;
         }).collect(Collectors.toList());
         return customerUserListVoList;
+    }
+    public Integer queryCount(CustomerUserQueryRequest customerUserQueryRequest){
+        Integer customerId = customerService.obtainCustomerId(false);
+        CustomerUserPo queryPo = new CustomerUserPo();
+        if(null != customerUserQueryRequest){
+            BeanUtils.copyProperties(customerUserQueryRequest,queryPo);
+        }
+        if (customerUserQueryRequest.getCustomerId() == null) {
+            queryPo.setCustomerId(customerId);
+        }
+        Integer cutomerUserCount = this.customerUserMapper.selectCount(queryPo);
+        return cutomerUserCount;
     }
 }
