8419eb5a677fbc1c1873f9718108f5273ff92d76
Strawberry157 <997008397@qq.com>
2018-11-23 18:09:58 +0800

    查询单个计划返回模型id，名称。设备id  名称

--- base/src/main/java/com/huanke/iot/base/dao/project/PlanMapper.java
+++ base/src/main/java/com/huanke/iot/base/dao/project/PlanMapper.java
@@ -2,6 +2,7 @@ package com.huanke.iot.base.dao.project;
 
 import com.huanke.iot.base.dao.BaseMapper;
 import com.huanke.iot.base.po.project.ProjectPlanInfo;
+import com.huanke.iot.base.resp.project.PlanInfoRsp;
 import com.huanke.iot.base.resp.project.PlanRspPo;
 import org.apache.ibatis.annotations.Param;
 
@@ -25,4 +26,6 @@ public interface PlanMapper extends BaseMapper<ProjectPlanInfo> {
     List<PlanRspPo> maintenance(@Param("projectId") Integer projectId, @Param("start") int start, @Param("limit") int limit);
 
     Integer selectMaintenanceCount(@Param("projectId")Integer projectId);
+
+    PlanInfoRsp  selectPlanInfoById(@Param("planId") Integer planId);
 }
