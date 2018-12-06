4b657c9f4b2a48b571f0bc3dbfc0c027eded9267
Strawberry157 <997008397@qq.com>
2018-12-06 18:12:45 +0800

    添加修改工程执行项

--- manage/src/main/java/com/huanke/iot/manage/service/project/ImplService.java
+++ manage/src/main/java/com/huanke/iot/manage/service/project/ImplService.java
@@ -1,7 +1,9 @@
 package com.huanke.iot.manage.service.project;
 
 import com.alibaba.fastjson.JSONObject;
+import com.huanke.iot.base.dao.DictMapper;
 import com.huanke.iot.base.dao.project.ImplMapper;
+import com.huanke.iot.base.po.config.DictPo;
 import com.huanke.iot.base.po.project.ProjectImplementLog;
 import com.huanke.iot.base.po.user.User;
 import com.huanke.iot.base.request.project.ImplementRequest;
@@ -12,10 +14,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 描述:
@@ -31,19 +30,49 @@ public class ImplService {
     private UserService userService;
     @Autowired
     private ImplMapper implMapper;
+    @Autowired
+    private DictMapper dictMapper;
 
     public Boolean addImplement(ImplementRequest request) {
         User user = userService.getCurrentUser();
         ProjectImplementLog projectImplementLog = new ProjectImplementLog();
         BeanUtils.copyProperties(request, projectImplementLog);
-        //处理图册
-        List<String> imgList = request.getImgList();
-        String imgListStr = String.join(",", imgList);
-        projectImplementLog.setImgList(imgListStr);
-        projectImplementLog.setFileList(JSONObject.toJSONString(request.getFileMap()));
-        projectImplementLog.setCreateUser(user.getId());
-        projectImplementLog.setCreateTime(new Date());
-        return implMapper.insert(projectImplementLog) > 0;
+        if(projectImplementLog.getId() == null){
+            //处理图册{
+            List<String> imgList = request.getImgList();
+            String imgListStr = String.join(",", imgList);
+            projectImplementLog.setImgList(imgListStr);
+            projectImplementLog.setFileList(JSONObject.toJSONString(request.getFileMap()));
+            projectImplementLog.setCreateUser(user.getId());
+            projectImplementLog.setCreateTime(new Date());
+            return implMapper.insert(projectImplementLog) > 0;
+        }else{
+            //处理图册{
+            List<String> imgList = request.getImgList();
+            String imgListStr = String.join(",", imgList);
+            projectImplementLog.setImgList(imgListStr);
+            List<ImplementRequest.updateFileMap> fileMapUpdate = request.getFileMapUpdate();
+            Map<String, List<String>> fileMap = new HashMap<>();
+            Set<String> keySet = fileMap.keySet();
+            for (ImplementRequest.updateFileMap fileUpdate : fileMapUpdate) {
+                Integer id = fileUpdate.getId();
+                DictPo dictPo = dictMapper.selectById(id);
+                String label = dictPo.getLabel();
+                if(keySet.contains(label)){
+                    List<String> stringList = fileMap.get(label);
+                    stringList.add(fileUpdate.getUrl());
+                }else{
+                    List<String> fileMapUp = new ArrayList<>();
+                    fileMapUp.add(fileUpdate.getUrl());
+                    fileMap.put(label, fileMapUp);
+                }
+            }
+            projectImplementLog.setFileList(JSONObject.toJSONString(fileMap));
+            projectImplementLog.setUpdateUser(user.getId());
+            projectImplementLog.setUpdateTime(new Date());
+            return implMapper.updateById(projectImplementLog) > 0;
+        }
+
     }
 
     public List<ImplementRsp> selectImplements(Integer projectId) {
