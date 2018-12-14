1d15cc1a82382a5d5e42b98301af53881a35df3f
Strawberry157 <997008397@qq.com>
2018-12-14 14:55:34 +0800

    添加H5设备组托管接口

--- api/src/main/java/com/huanke/iot/api/controller/h5/DeviceTeamController.java
+++ api/src/main/java/com/huanke/iot/api/controller/h5/DeviceTeamController.java
@@ -3,6 +3,7 @@ package com.huanke.iot.api.controller.h5;
 import com.alibaba.fastjson.JSON;
 import com.huanke.iot.api.controller.h5.req.BaseRequest;
 import com.huanke.iot.api.controller.h5.req.OccRequest;
+import com.huanke.iot.api.controller.h5.req.TeamTrusteeRequest;
 import com.huanke.iot.api.controller.h5.req.UpdateDeviceTeamRequest;
 import com.huanke.iot.api.controller.h5.response.DeviceTeamVo;
 import com.huanke.iot.api.controller.h5.team.DeviceTeamNewRequest;
@@ -10,12 +11,15 @@ import com.huanke.iot.api.controller.h5.team.DeviceTeamRequest;
 import com.huanke.iot.api.service.device.team.DeviceTeamService;
 import com.huanke.iot.base.api.ApiResponse;
 import com.huanke.iot.base.constant.RetCode;
+import com.huanke.iot.base.po.customer.CustomerUserPo;
 import com.huanke.iot.base.po.device.team.DeviceTeamPo;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -111,5 +115,28 @@ public class DeviceTeamController extends BaseController {
         log.info("组开关操作，操作人={}, 操作状态={}，操作组={}", userId, openCloseStatus, teamId);
         Boolean result = deviceTeamService.occ(occRequest, userId, 1);
         return new ApiResponse<>(result);
+    }
+
+    @ApiOperation("通过指定用户openId托管组给另一用户")
+    @RequestMapping(value = "/trusteeTeam",method = RequestMethod.POST)
+    public ApiResponse<Integer> trusteeTeam(@RequestBody TeamTrusteeRequest teamTrusteeRequest){
+        //首先查询要托管的用户是否存在
+        try {
+
+            CustomerUserPo queryCustomerUserPo = this.deviceTeamService.queryCustomerUser(teamTrusteeRequest.getOpenId());
+            if (queryCustomerUserPo==null){
+                return new ApiResponse<>(RetCode.PARAM_ERROR,"当前用户 "+teamTrusteeRequest.getOpenId()+" 不存在");
+            }
+            CustomerUserPo customerUserPo=this.deviceTeamService.trusteeTeam(teamTrusteeRequest);
+            if(null != customerUserPo){
+                return new ApiResponse<>(RetCode.OK,"托管成功",customerUserPo.getId());
+            }
+            else {
+                return new ApiResponse<>(RetCode.ERROR,"托管失败");
+            }
+        } catch (Exception e) {
+            log.error("托管组失败={}", e);
+            return new ApiResponse<>(RetCode.ERROR, "托管组失败");
+        }
     }
 }
---
+++ api/src/main/java/com/huanke/iot/api/controller/h5/req/TeamTrusteeRequest.java
@@ -0,0 +1,15 @@
+package com.huanke.iot.api.controller.h5.req;
+
+import lombok.Data;
+
+/**
+ * 描述:
+ *
+ * @author onlymark
+ * @create 2018-12-14 下午2:41
+ */
+@Data
+public class TeamTrusteeRequest {
+    private Integer teamId;
+    private String openId;
+}
