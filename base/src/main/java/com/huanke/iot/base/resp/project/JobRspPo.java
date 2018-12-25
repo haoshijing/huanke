080657db5e72ceccf76648147e9ae2e6374bc40b
Strawberry157 <997008397@qq.com>
2018-12-25 16:03:12 +0800

    任务列表返回添加关联类型

--- base/src/main/java/com/huanke/iot/base/resp/project/JobRspPo.java
+++ base/src/main/java/com/huanke/iot/base/resp/project/JobRspPo.java
@@ -14,6 +14,7 @@ import java.util.Date;
 public class JobRspPo {
     private Integer id;
     private String name;
+    private Integer type;
     private String description;
     private String ruleName;
     private String projectTypeName;
