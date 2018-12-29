38f5a1fffa72565a58ef1757a3d2750c2c9ca21d
997008397@qq.com <997008397@qq.com>
2018-12-29 14:42:37 +0800

    添加设备报警逻辑

--- job/src/main/java/com/huanke/iot/job/stat/DeviceSensorStatWorker.java
+++ job/src/main/java/com/huanke/iot/job/stat/DeviceSensorStatWorker.java
@@ -3,9 +3,14 @@ package com.huanke.iot.job.stat;
 import com.huanke.iot.base.dao.device.DeviceMapper;
 import com.huanke.iot.base.dao.device.data.DeviceSensorDataMapper;
 import com.huanke.iot.base.dao.device.stat.DeviceSensorStatMapper;
+import com.huanke.iot.base.dao.device.stat.DeviceSensorWarnMapper;
+import com.huanke.iot.base.dao.device.typeModel.DeviceModelMapper;
+import com.huanke.iot.base.dao.project.JobMapper;
 import com.huanke.iot.base.enums.SensorTypeEnums;
 import com.huanke.iot.base.po.device.DevicePo;
 import com.huanke.iot.base.po.device.stat.DeviceSensorStatPo;
+import com.huanke.iot.base.po.device.stat.DeviceSensorWarnPo;
+import com.huanke.iot.base.po.project.ProjectJobInfo;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.DefaultThreadFactory;
 import lombok.AllArgsConstructor;
@@ -13,9 +18,11 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
@@ -31,11 +38,18 @@ public class DeviceSensorStatWorker {
 
     @Autowired
     private DeviceMapper deviceMapper;
+    @Autowired
+    private DeviceSensorWarnMapper deviceSensorWarnMapper;
+    @Autowired
+    private JobMapper jobMapper;
 
     @Autowired
     private DeviceSensorDataMapper deviceSensorDataMapper;
     @Autowired
     private DeviceSensorStatMapper deviceSensorStatMapper;
+    @Autowired
+    private RedisTemplate<Object,Object> template;
+
 
     private DefaultEventExecutorGroup defaultEventExecutorGroup = new DefaultEventExecutorGroup(16,
             new DefaultThreadFactory("StatSensorThread"));
@@ -50,6 +64,11 @@ public class DeviceSensorStatWorker {
         final Long endTime = new DateTime().withMillisOfSecond(0).plusMinutes(-10).getMillis();
         do {
             devicePoList.forEach(devicePo -> {
+                //未被分配的设备不记录
+                Integer customerId = devicePo.getCustomerId();
+                if(customerId == null){
+                    return;
+                }
                 Future<Integer> co2 = defaultEventExecutorGroup.submit(new QueryTask(devicePo.getId(), startTime, endTime, SensorTypeEnums.CO2_IN.getCode()));
                 Future<Integer>  hcho = defaultEventExecutorGroup.submit(new QueryTask(devicePo.getId(),startTime,endTime,SensorTypeEnums.HCHO_IN.getCode()));
                 Future<Integer>  tvoc = defaultEventExecutorGroup.submit(new QueryTask(devicePo.getId(),startTime,endTime,SensorTypeEnums.TVOC_IN.getCode()));
@@ -73,6 +92,10 @@ public class DeviceSensorStatWorker {
                     deviceSensorStatPo.setTem(tem.get());
                     deviceSensorStatPo.setNh3(nh3.get());
                     deviceSensorStatPo.setAnion(anion.get());
+                    //根据传感器数值大小，判断是否设备报警
+                    if(customerId == 3 && devicePo.getPowerStatus() == 1){//韩师傅 和 开机设备开启设备报警
+                        deviceWarn(devicePo.getId(), customerId, deviceSensorStatPo);
+                    }
                 }catch (Exception e){
                     log.error("",e);
                 }
@@ -87,6 +110,67 @@ public class DeviceSensorStatWorker {
             devicePoList = deviceMapper.selectList(queryPo, 100, offset);
         } while (devicePoList.size() > 0);
         log.info("DeviceSensorStatWorker end work");
+    }
+
+    private void deviceWarn(Integer deviceId, Integer customerId, DeviceSensorStatPo deviceSensorStatPo) {
+        DeviceSensorWarnPo deviceSensorWarnPo = (DeviceSensorWarnPo)template.opsForValue().get("sensorWarn" + customerId);
+        if(deviceSensorWarnPo == null){
+            deviceSensorWarnPo = deviceSensorWarnMapper.selectByCustomerId(customerId);
+            if(deviceSensorWarnPo == null){
+                log.info("该公众号没有配置");
+                return;
+            }
+            template.opsForValue().set("sensorWarn" + customerId, deviceSensorWarnPo);
+        }
+        StringBuilder sb = new StringBuilder();
+        //判断温度
+        Integer temMax = deviceSensorWarnPo.getTemMax();
+        if(deviceSensorStatPo.getTem() > temMax){
+            sb.append("温度过高报警。");
+        }
+        Integer temMin = deviceSensorWarnPo.getTemMin();
+        if(deviceSensorStatPo.getTem() < temMin){
+            sb.append("温度过低报警。");
+        }
+        Integer humMax = deviceSensorWarnPo.getHumMax();
+        if(deviceSensorStatPo.getHum() > humMax){
+            sb.append("湿度过高报警。");
+        }
+        Integer humMin = deviceSensorWarnPo.getHumMin();
+        if(deviceSensorStatPo.getHum() < humMin){
+            sb.append("湿度过低报警。");
+        }
+        Integer pm = deviceSensorWarnPo.getPm();
+        if(deviceSensorStatPo.getPm() > pm){
+            sb.append("PM过高报警。");
+        }
+        Integer hcho = deviceSensorWarnPo.getHcho();
+        if(deviceSensorStatPo.getHcho() > hcho){
+            sb.append("甲醛过高报警。");
+        }
+        Integer tvoc = deviceSensorWarnPo.getTvoc();
+        if(deviceSensorStatPo.getTvoc() > tvoc){
+            sb.append("TVOC过高报警。");
+        }
+        Integer co2 = deviceSensorWarnPo.getCo2();
+        if(deviceSensorStatPo.getCo2() > co2){
+            sb.append("CO2报警。");
+        }
+        if(!sb.toString().equals("")){
+            ProjectJobInfo projectJobInfo = new ProjectJobInfo();
+            projectJobInfo.setCustomerId(customerId);
+            projectJobInfo.setLinkDeviceId(deviceId);
+            projectJobInfo.setName("设备报警");
+            projectJobInfo.setDescription(sb.toString());
+            projectJobInfo.setIsRule(0);
+            projectJobInfo.setWarnLevel(3);
+            projectJobInfo.setSourceType(3);
+            projectJobInfo.setWarnStatus(2);
+            projectJobInfo.setFlowStatus(1);
+            projectJobInfo.setStatus(1);
+            projectJobInfo.setCreateTime(new Date());
+            jobMapper.insert(projectJobInfo);
+        }
     }
 
     @AllArgsConstructor
