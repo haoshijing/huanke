af77902474dedafaa9e5a803edcc57f0f1366ddd
zdy <965082472@qq.com>
2018-12-31 14:38:41 +0800

    设备告警分类、

--- base/src/main/java/com/huanke/iot/base/dao/project/JobMapper.java
+++ base/src/main/java/com/huanke/iot/base/dao/project/JobMapper.java
@@ -21,7 +21,7 @@ public interface JobMapper extends BaseMapper<ProjectJobInfo> {
 
     Boolean batchDelete(@Param("userId") Integer userId, @Param("valueList") List<Integer> valueList);
 
-    List<JobRspPo> selectPageList(@Param("projectJobInfo") ProjectJobInfo projectJobInfo, @Param("start") int start, @Param("limit") int limit, @Param("userId") Integer userId);
+    List<JobRspPo> selectPageList(@Param("projectJobInfo") ProjectJobInfo projectJobInfo, @Param("start") int start, @Param("limit") int limit, @Param("userId") Integer userId, @Param("projectName") String projectName);
 
     //Boolean batchForbidden(@Param("userId") Integer userId, @Param("valueList") List<Integer> valueList);
 
